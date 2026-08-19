945c79afd7feb6c2669473028ae690a664e5f8e3
Sofchka <sofizaqar@gmail.com>
2026-08-19 09:25:05 +0400

    proccess managament task added

---
+++ Homework3/math/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace Math;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+            throw new Exception("Wrong Argument wrong usage");
+        string operation = "";
+        double a1 = 0;
+        double a2 = 0;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "--add":
+                    operation = "add";
+                    break;
+
+                case "--mult":
+                    operation = "mult";
+                    break;
+
+                case "--sub":
+                    operation = "sub";
+                    break;
+
+                case "--div":
+                    operation = "div";
+                    break;
+
+                case "--a1":
+                    a1 = double.Parse(args[++i]);
+                    break;
+
+                case "--a2":
+                    a2 = double.Parse(args[++i]);
+                    break;
+            }
+        }
+
+        double result = operation switch
+        {
+            "add" => a1 + a2,
+            "mult" => a1 * a2,
+            "sub" => a1 - a2,
+            "div" => a1 / a2,
+            _ => throw new ArgumentException("Unknown operation")
+        };
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Homework3/proc/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MathMultiThreading;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo start = new ProcessStartInfo()
+        {
+            FileName = "/Users/sofi/alo123/Homework3/math/bin/Debug/net10.0/math",
+            Arguments = "--add --a1 12 --a2 13",
+            UseShellExecute = false,
+            WorkingDirectory = Environment.CurrentDirectory,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+
+        Process process = new Process()
+        {
+            StartInfo = start
+        };
+
+        process.Start();
+        var output = process.StandardOutput.ReadToEnd();
+        var outputerr = process.StandardError.ReadToEnd();
+
+        Console.WriteLine(output);
+        Console.WriteLine(outputerr);
+    }
+}
